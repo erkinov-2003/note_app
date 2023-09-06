cdce48dcd465fcef64b4acd7ff076c30131d0b24
Miracle-Blue <miracle.blue.com@gmail.com>
2023-09-06 19:56:39 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/miracle/note_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.miracle.note_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+import 'src/common/widget/app.dart';
+
+void main() => runApp(const App());
+
