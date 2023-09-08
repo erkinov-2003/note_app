6ecfaa9b9d0bd7db421ac122a41bafb1e91d708d
Ibrohim <ibrohimjonodilov28@gmail.com>
2023-09-08 15:56:52 +0500

    create note

--- lib/src/common/widget/app.dart
+++ lib/src/common/widget/app.dart
@@ -5,6 +5,9 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Note App",
+    );
   }
 }
