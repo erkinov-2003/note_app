1778049002dcd90eef6bc5f63e82803c3b6c7b5b
erkinovs-blog <murodjonerkinov2005@gmail.com>
2023-09-13 15:22:05 +0500

    first commit

--- lib/src/common/widget/app.dart
+++ lib/src/common/widget/app.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/src/features/welcome_page/widget/welcome_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Note App",
+      theme: ThemeData(
+        useMaterial3: true,
+        fontFamily: "Ranade"
+      ),
+      darkTheme: ThemeData.dark(useMaterial3: true),
+      home: const WelcomePage(),
+    );
   }
 }
