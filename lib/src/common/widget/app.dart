5fced92b52981783db19e27d002835c11b48ca96
Miracle-Blue <miracle.blue.com@gmail.com>
2023-09-18 17:45:45 +0500

    added localization

--- lib/src/common/widget/app.dart
+++ lib/src/common/widget/app.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:note_app/src/common/localization/generated/l10n.dart';
 
 import '../../features/splash_screen/widget/splash_screen.dart';
 
@@ -14,6 +16,13 @@ class App extends StatelessWidget {
         useMaterial3: true,
         fontFamily: "Ranade",
       ),
+      localizationsDelegates: const <LocalizationsDelegate<Object>>[
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        GeneratedLocalization.delegate,
+      ],
+      supportedLocales: GeneratedLocalization.delegate.supportedLocales,
       darkTheme: ThemeData.dark(useMaterial3: true),
       home: const SplashScreen(),
     );
