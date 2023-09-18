25da599ba441d6714e2f3bdd658e563efb143055
Miracle-Blue <miracle.blue.com@gmail.com>
2023-09-18 18:15:30 +0500

    changed dependencies

--- lib/src/common/models/dependencies.dart
+++ lib/src/common/models/dependencies.dart
@@ -1,25 +1,39 @@
 import 'package:flutter/cupertino.dart';
-import 'package:note_app/src/common/initialize/inherited_dependencies.dart';
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+import '../initialize/inherited_dependencies.dart';
 
 abstract interface class Dependencies {
   static Dependencies of(BuildContext context) => InheritedDependencies.of(context);
 
   abstract final SharedPreferences storage;
+  abstract final FlutterSecureStorage secureStorage;
 }
 
 final class MutableDependencies implements Dependencies {
   @override
   late SharedPreferences storage;
 
+  @override
+  late FlutterSecureStorage secureStorage;
+
   Dependencies freeze() => ImmutableDependencies(
         storage: storage,
+        secureStorage: secureStorage,
       );
 }
 
 final class ImmutableDependencies implements Dependencies {
-  const ImmutableDependencies({required this.storage});
+  const ImmutableDependencies({
+    required this.storage,
+    required this.secureStorage,
+  });
 
   @override
   final SharedPreferences storage;
+
+  @override
+  final FlutterSecureStorage secureStorage;
 }
