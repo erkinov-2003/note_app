b3ea48852a51834e159d40987d7059292c3abed2
YunusovJaloliddin <yunusovjaloliddin16112001@gmail.com>
2023-09-19 15:47:20 +0500

    forgot connect login

--- lib/src/features/forgot_password/widget/chenge_password.dart
+++ lib/src/features/forgot_password/widget/chenge_password.dart
@@ -132,6 +132,10 @@ class _ChangePasswordState extends State<ChangePassword> {
                         padding: EdgeInsets.only(top: size.height * 0.20),
                         child: ElevatedButton(
                           style: ButtonStyle(
+                            backgroundColor:
+                            const MaterialStatePropertyAll<Color>(
+                              Color(0xFF36BFFA),
+                            ),
                             fixedSize: MaterialStatePropertyAll<Size>(
                               Size(
                                 size.width,
@@ -158,6 +162,7 @@ class _ChangePasswordState extends State<ChangePassword> {
                             style: TextStyle(
                               fontWeight: FontWeight.w600,
                               fontSize: size.height * 0.019,
+                              color: Colors.white,
                             ),
                           ),
                         ),
