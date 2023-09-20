ed9ed356598785ae154d6cde58701fe50168b80f
shoxruxtm <shoxrux20040803@gmail.com>
2023-09-20 15:51:02 +0500

    language_add

---
+++ lib/src/common/localization/generated/intl/messages_uz.dart
@@ -0,0 +1,62 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a uz locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'uz';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "addPassword": MessageLookupByLibrary.simpleMessage("Parol Qo\'shish"),
+        "addPhoto": MessageLookupByLibrary.simpleMessage("Fotosurat Qo\'shing"),
+        "back": MessageLookupByLibrary.simpleMessage("Ortga"),
+        "delete": MessageLookupByLibrary.simpleMessage("O\'chirish"),
+        "emailAddress":
+            MessageLookupByLibrary.simpleMessage("Elektron pochta manzili"),
+        "enterEmailAddress": MessageLookupByLibrary.simpleMessage(
+            "Elektron pochta manzilini kiriting"),
+        "forgotPassword":
+            MessageLookupByLibrary.simpleMessage("Parolni unutdingizmi?"),
+        "fullName": MessageLookupByLibrary.simpleMessage("To\'liq ism"),
+        "language": MessageLookupByLibrary.simpleMessage("Til"),
+        "languageApp": MessageLookupByLibrary.simpleMessage("Til ilovasi"),
+        "logIn": MessageLookupByLibrary.simpleMessage("Kirish"),
+        "logOut": MessageLookupByLibrary.simpleMessage("Tizimdan chiqish"),
+        "newNote": MessageLookupByLibrary.simpleMessage("Yangi eslatma"),
+        "next": MessageLookupByLibrary.simpleMessage("Keyingi"),
+        "openCamera": MessageLookupByLibrary.simpleMessage("Camera Ochish"),
+        "openGallery": MessageLookupByLibrary.simpleMessage("Galareya Ochish"),
+        "password": MessageLookupByLibrary.simpleMessage("Parol"),
+        "profile": MessageLookupByLibrary.simpleMessage("Profil"),
+        "profileImage": MessageLookupByLibrary.simpleMessage("Profil Rasmi"),
+        "register": MessageLookupByLibrary.simpleMessage("Ro\'yxatdan o\'tish"),
+        "secretPassword":
+            MessageLookupByLibrary.simpleMessage("Maxfiy eslatma uchun parol"),
+        "setPassword":
+            MessageLookupByLibrary.simpleMessage("Parolni o\'rnating"),
+        "signIn": MessageLookupByLibrary.simpleMessage("Tizimga kirish"),
+        "signInTitle": MessageLookupByLibrary.simpleMessage(
+            "ESLATMANGIZNI qaytarib olishga xush kelibsiz..."),
+        "signUp": MessageLookupByLibrary.simpleMessage("Imzolash"),
+        "signUpTitle": MessageLookupByLibrary.simpleMessage(
+            "Sizning eslatmangiz bu erda..."),
+        "theme": MessageLookupByLibrary.simpleMessage("Tema"),
+        "themeApp": MessageLookupByLibrary.simpleMessage("Mavzu Ilovasi"),
+        "yourName": MessageLookupByLibrary.simpleMessage("Sizning Ismingiz")
+      };
+}
