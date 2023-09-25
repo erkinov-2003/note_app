a0462a5ffedc9e8b9ae0ddda172785905474a59d
erkinovs-blog <murodjonerkinov2005@gmail.com>
2023-09-25 16:38:42 +0500

    localization added

--- lib/src/common/widget/app.dart
+++ lib/src/common/widget/app.dart
@@ -1,15 +1,10 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:note_app/src/features/create_note/create_note.dart';
 import 'package:note_app/src/features/home_screen/home_page.dart';
 import '../../features/splash_screen/widget/splash_screen.dart';
 import '../localization/generated/l10n.dart';
-import '../models/user_model.dart';
-import 'package:note_app/src/common/localization/generated/l10n.dart';
+import 'package:note_app/src/common/providers/lang_provider.dart';
 import 'package:note_app/src/common/utils/storage.dart';
-import 'package:note_app/src/features/home_screen/home_page.dart';
-import 'package:note_app/src/features/splash_screen/widget/splash_screen.dart';
 import 'package:provider/provider.dart';
 
 class App extends StatelessWidget {
@@ -17,24 +12,36 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider.value(
-      value: $notes,
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: "Note App",
-        theme: ThemeData(
-          useMaterial3: true,
-          fontFamily: "Ranade",
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(value: $notes),
+        ChangeNotifierProvider(
+          create: (context) => LangProvider(),
         ),
-        localizationsDelegates: const <LocalizationsDelegate<Object>>[
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-          GlobalCupertinoLocalizations.delegate,
-          GeneratedLocalization.delegate,
-        ],
-        supportedLocales: GeneratedLocalization.delegate.supportedLocales,
-        darkTheme: ThemeData.dark(useMaterial3: true),
-        home: $notes.isLogged == "true" ? const HomePage() :  const SplashScreen(),
+      ],
+      child: Consumer<LangProvider>(
+        builder: (context, value, child) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: "Note App",
+            theme: ThemeData(
+              useMaterial3: true,
+              fontFamily: "Ranade",
+            ),
+            localizationsDelegates: const <LocalizationsDelegate<Object>>[
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+              GeneratedLocalization.delegate,
+            ],
+            supportedLocales: GeneratedLocalization.delegate.supportedLocales,
+            darkTheme: ThemeData.dark(useMaterial3: true),
+            home: $notes.isLogged == "true"
+                ? const HomePage()
+                : const SplashScreen(),
+            locale: value.getLocale(),
+          );
+        },
       ),
     );
   }
